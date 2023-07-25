bebd896b72c8713881aa14a0219b83cd90e4df62
황진주 <vjinjoov@naver.com>
2023-07-25 21:12:23 +0900

    [FEAT]Main.kt/기본소지금및이름만입력하도록함.User.kt/나머지 주문은 user.kt에서 할 수 있도록 수정

--- Kiosk/app/src/main/java/com/team8/kiosk/Main.kt
+++ Kiosk/app/src/main/java/com/team8/kiosk/Main.kt
@@ -1,5 +1,19 @@
 package com.team8.kiosk
 
-fun main(){
-    println("dd")
+
+fun main() {
+    println("고객님 이름을 입력해주세요.")
+    val userName = readLine()?: ""
+    println("고객님의 소지금을 입력해주세요.(숫자만 입력해주세요.)")
+    var remainMoney = readLine()?.toIntOrNull() ?: 0
+
+    if (remainMoney<1000){
+    println("죄송합니다 고객님, 소지금으로 살 수 있는 상품이 없습니다.충전하실 금액을 입력해주세요.")
+    var addMoney =  readLine()?.toIntOrNull() ?: 0
+        remainMoney += addMoney
+
+}
+    val user: User = User(userName, remainMoney)
+    user.shopping()
+
 }
--- Kiosk/app/src/main/java/com/team8/kiosk/User.kt
+++ Kiosk/app/src/main/java/com/team8/kiosk/User.kt
@@ -2,13 +2,49 @@ package com.team8.kiosk
 
 class User(
     val name: String, // 사용자 이름
-    var remainMoney: Int, // 남은 돈
+    var remainMoney: Int // 남은 돈
+
 ) {
     private val carts: MutableList<Cart> = mutableListOf() // 장바구니
 
     fun shopping() { // 쇼핑하기 로직
+        println("${name}님 ! 맥도날드에 ${remainMoney}원을 가지고 오신 것을 환영합니다. 주문을 선택해주세요.")
+        println("[1]포장 , [2]매장식사")
+    val sel = readLine()?.toIntOrNull()
 
+    if (sel in 1..2) {
+        if (sel == 1) {
+            println("1.포장을 선택하셨습니다. 메뉴 선택화면으로 넘어갑니다.")
+            val sel1 = "[포장]"
+            // 포장 영수증 출력 로직 (sel1 변수 활용)
+        } else if (sel == 2) {
+            println("2.매장식사를 선택하셨습니다. 메뉴 선택화면으로 넘어갑니다.")
+            val sel2 = "[매장식사]"
+            // 매장식사 영수증 출력 로직 (sel2 변수 활용)
+        }
+
+
+    } else {
+        println("잘못된 선택입니다. 1 또는 2를 입력해주세요. (1.포장 2.매장식사)")
     }
+        //메뉴 선택화면
+        println("메뉴 선택 : [1]버거 [2]음료 [3]사이드")
+        var menuList = readLine()?.toIntOrNull()
+        if (menuList in 1..3) {
+            when(menuList) {
+                1 -> {
+                    println("버거를 선택하셨습니다.")
+
+                    //버거정보 출력
+                    val burgers= Menu.menuArr[0]
+                    for ((index, burger) in burgers.withIndex()) {
+                        println("${index + 1}. ${burger.name} - ${burger.price}원")
+                    }
+                }
+            }
+        }
+    }
+
 
     fun purchase() { // 구매 로직
 
@@ -24,8 +60,5 @@ class User(
 
     fun updateCart(index: Int) { // 카드 수정
 
-    }
-    fun remainingMoney(): Int {  //추가함..ㅠㅠ..(진주)
-        return remainMoney
     }
 }
